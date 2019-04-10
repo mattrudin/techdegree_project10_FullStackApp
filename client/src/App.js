a15463e9eca01b1692c6c5fd904381850d4906a6
Matt Rudin <matthias.rudin@live.com>
2019-04-10 17:59:50 +0200

    Client to API connection works

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: []
+  }
+
+  async componentDidMount() {
+    const url = 'http://localhost:5000/api/courses'
+    try {
+      const rawData = await fetch(url)
+      const data = await rawData.json()
+      this.setState({ data })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>List of all courses</h1>
+          {this.state.data.map(course => <h2>{course.title}</h2>)}
         </header>
       </div>
     );
