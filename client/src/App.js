18337c906da2b21ae38fa5954882cc6115b70e54
Matt Rudin <matthias.rudin@live.com>
2019-04-12 17:37:44 +0200

    Add routes to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './global.css'
-import Header from './components/Header'
 
-// For development purpose only!
-// import Courses from './components/Courses'
-// import CourseDetail from './components/CourseDetail'
-// import UserSignIn from './components/UserSignIn'
-// import UserSignUp from './components/UserSignUp'
-// import CreateCourse from './components/CreateCourse'
+// App components
+import Header from './components/Header'
+import Courses from './components/Courses'
+import CreateCourse from './components/CreateCourse'
 import UpdateCourse from './components/UpdateCourse'
+import CourseDetail from './components/CourseDetail'
+import UserSignUp from './components/UserSignUp'
+import UserSignIn from './components/UserSignIn'
+import UserSignOut from './components/UserSignOut'
+
 
 class App extends Component {
   state = {
@@ -17,10 +20,16 @@ class App extends Component {
 
   render() {
     return (
-      <>
+      <BrowserRouter>
         <Header />
-        <UpdateCourse />
-      </>
+        <Route exact path="/" component={Courses} />
+        <Route path="/courses/create" component={CreateCourse} />
+        <Route path="/courses/:id/update" component={UpdateCourse} />
+        <Route path="/courses/:id" component={CourseDetail} />
+        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOut} />
+      </BrowserRouter>
     );
   }
 }
