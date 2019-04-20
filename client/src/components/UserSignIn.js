d8a49249d0e09dc1ecea387eaa7e56d4daadd1b2
Matt Rudin <matthias.rudin@live.com>
2019-04-20 15:28:42 +0200

    Fix autocomplete warning in UserSignIn component

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -17,6 +17,7 @@ export default class UserSignIn extends Component {
         return (
             <Consumer>
                 { context => {
+                    const { correctAuth, isLoggedIn } = this.state
 
                     const onSubmit = async (values) => {
                         const { emailAddress, password } = values
@@ -35,40 +36,53 @@ export default class UserSignIn extends Component {
                         }
                     }
 
-                    const { correctAuth, isLoggedIn } = this.state
+                    const LoggedInView = () => (
+                        <div className="grid-66 centered signin">
+                            <h1>Successfully logged in!</h1>
+                            <Link className="button" to="/" >To course list</Link>
+                        </div>
+                    )
+
+                    const ValidationView = () => (
+                        <div className="validation-errors">
+                            <ul>
+                                <li>Email or password is incorrect.</li>
+                            </ul>
+                        </div>
+                    )
+
+                    const NotLoggedInView = () => (
+                        <div className="grid-33 centered signin">
+                            <h1>Sign In</h1>
+                            {
+                                !correctAuth &&
+                                <ValidationView />
+                            }
+                            <Form
+                                onSubmit={onSubmit}
+                                render={ ({ handleSubmit, values }) => (
+                                    <form onSubmit={handleSubmit}>
+                                        <Field name="emailAddress" component="input" placeholder="Email Address" />
+                                        <Field name="password" component="input" type="password" placeholder="Password" autoComplete="password" />
+                                        <div className="grid-100 pad-bottom">
+                                            <Link className="button" onClick={handleSubmit} to="" >Sign In</Link>
+                                            <Link className="button button-secondary" to="/" >Cancel</Link>
+                                        </div>
+                                    </form>
+                                )}
+                            />
+                            <p>&nbsp;</p>
+                            <p>Don't have a user account? <Link to="signup">Click here</Link> to sign up!</p>
+                        </div>
+                    )
                     
                     return (
                         <div className="bounds">
-                            {!isLoggedIn && 
-                            <div className="grid-33 centered signin">
-                                <h1>Sign In</h1>
-                                {!correctAuth &&
-                                <div className="validation-errors">
-                                    <ul>
-                                        <li>Email or password is incorrect.</li>
-                                    </ul>
-                                </div>}
-                                <Form
-                                    onSubmit={onSubmit}
-                                    render={ ({ handleSubmit, values }) => (
-                                        <form onSubmit={handleSubmit}>
-                                            <Field name="emailAddress" component="input" placeholder="Email Address" />
-                                            <Field name="password" component="input" type="password" placeholder="Password" />
-                                            <div className="grid-100 pad-bottom">
-                                                <Link className="button" onClick={handleSubmit} >Sign In</Link>
-                                                <Link className="button button-secondary" to="/" >Cancel</Link>
-                                            </div>
-                                        </form>
-                                    )}
-                                />
-                                <p>&nbsp;</p>
-                                <p>Don't have a user account? <Link to="signup">Click here</Link> to sign up!</p>
-                            </div>}
-                            {isLoggedIn &&
-                            <div className="grid-33 centered signin">
-                                <h1>Successfully logged in!</h1>
-                                <Link className="button" to="/" >To course list</Link>
-                            </div>}
+                            {
+                                isLoggedIn ?
+                                <LoggedInView /> :
+                                <NotLoggedInView />
+                            }
                         </div>
                     )}
                 }
