50f4aee6be3121195584cc84ff159de149fc66e0
Matt Rudin <matthias.rudin@live.com>
2019-04-20 11:03:14 +0200

    Update Header component with context

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Consumer } from './Context'
 
-const Header = (props) => {
-    // const { isLoggedId } = props
-    return (
-        <header className="header">
-            <div className="bounds">
-                <Link to="/">
-                    <h1 className="header--logo">Courses</h1>
-                </Link>
+const Header = () => (
+    <Consumer>
+        { context => {
+            const { isLoggedIn, firstName, lastName } = context.user
+
+            const LoggedInNav = () => (
+                <nav>
+                    <span>Welcome {firstName} {lastName}</span>
+                    <Link to="/signout" className="signout">Sign Out</Link>
+                </nav>
+            )
+
+            const NotLoggedInNav = () => (
                 <nav>
                     <Link to="/signup" className="signup">Sign Up</Link>
                     <Link to="/signin" className="signin">Sign In</Link>
                 </nav>
-            </div>
-        </header>
-    )
-}
+            )
+
+            return(
+                <header className="header">
+                    <div className="bounds">
+                        <Link to="/">
+                            <h1 className="header--logo">Courses</h1>
+                        </Link>
+                        {
+                            isLoggedIn ?
+                            <LoggedInNav /> :
+                            <NotLoggedInNav />
+                        }
+                    </div>
+                </header>
+            )
+        }}
+    </Consumer>
+)
 
 export default Header
