a1ea8d013a34b52dedca222bfc257601a077b78a
Matt Rudin <matthias.rudin@live.com>
2019-04-21 13:14:34 +0200

    Update ActionBar to stateful component

--- client/src/components/ActionBar.js
+++ client/src/components/ActionBar.js
@@ -1,37 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import { deleteCourseWithID } from '../utility/CRUD'
 import { Consumer } from './Context'
 
-const ActionBar = (props) => {
-  const { courseID, user } = props.data
-  const { _id: courseUserId } = user
-
-  return (
-    <Consumer>
-      { context => {
-        const { id: authUserID, isLoggedIn, authHeader } = context.user
-        
-        return(
-          <div className="actions--bar">
-              <div className="bounds">
-                <div className="grid-100">
-                  { 
-                    isLoggedIn && 
-                    courseUserId === authUserID &&
-                    <span>
-                      <Link className="button" to={`/courses/${courseID}/update`}>Update Course</Link>
-                      <Link className="button" onClick={() => deleteCourseWithID(courseID, authHeader)} to="/" >Delete Course</Link>
-                    </span>
-                  }
-                  <Link className="button button-secondary" to="/">Return to List</Link>
+export default class ActionBar extends Component {
+  state = {
+    isDeleted: false,
+  }
+  
+  render() {
+    const { courseID, user } = this.props.data
+    const { isDeleted } = this.state
+    const { _id: courseUserId } = user
+    return (
+      <Consumer>
+        { context => {
+          const { id: authUserID, isLoggedIn, authHeader } = context.user
+  
+          const handleClick = async (event) => {
+            event.preventDefault()
+            await deleteCourseWithID(courseID, authHeader)
+            this.setState({
+              isDeleted: true,
+            })
+          }
+          
+          return(
+            <div className="actions--bar">
+                <div className="bounds">
+                  <div className="grid-100">
+                    { 
+                      isLoggedIn && 
+                      courseUserId === authUserID &&
+                      <span>
+                        <Link className="button" to={`/courses/${courseID}/update`}>Update Course</Link>
+                        <Link className="button" onClick={handleClick} to="" >Delete Course</Link>
+                      </span>
+                    }
+                    {
+                      isDeleted &&
+                      <Redirect push to="/" />
+                    }
+                    <Link className="button button-secondary" to="/">Return to List</Link>
+                  </div>
                 </div>
-              </div>
-          </div>
-        )
-      }}
-    </Consumer>
-  )
+            </div>
+          )
+        }}
+      </Consumer>
+    )
+  }
 }
-
-export default ActionBar
